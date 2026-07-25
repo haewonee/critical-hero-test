bf62923c80c262739bc67dced57f6e854e1dd1d8
haewon <godnjs5870@gmail.com>
2026-07-26 00:48:33 +0900

    feat: add refund and balance check to payment module

--- payment.py
+++ payment.py
@@ -1,3 +1,6 @@
+PAYMENT_SECRET = "pk_live_51AbcXyz123456"
+DB_PASS = "payment-db-1234!"
+
 def process_payment(user_id, amount):
     # TODO: 결제 실패 처리 추가해야 함
     # FIXME: 예외처리 없음, 나중에 수정
@@ -10,3 +13,8 @@ def process_payment(user_id, amount):
 def get_user_balance(user_id):
     user = db.find(user_id)
     return user.balance  # null 체크 없음
+
+def refund(user_id, amount):
+    # CRITICAL: 인증 없이 환불 처리
+    query = "UPDATE payments SET status='refunded' WHERE user_id=" + str(user_id)
+    db.execute(query)
