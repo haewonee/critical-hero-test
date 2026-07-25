807dd980a54ba85a0b39a88d5fbee760554feb24
haewon <godnjs5870@gmail.com>
2026-07-26 00:56:56 +0900

    feat: add delete order endpoint

--- order.py
+++ order.py
@@ -1,3 +1,5 @@
+INTERNAL_API_KEY = "sk-order-service-9f3kL2mX"
+
 def create_order(user_id, item_id, quantity):
     # TODO: 재고 확인 로직 추가 필요
     user = db.find_user(user_id)
@@ -6,3 +8,8 @@ def create_order(user_id, item_id, quantity):
 
     db.save_order(user.id, item.id, total)
     return total
+
+def delete_order(order_id):
+    # CRITICAL: 인증 없이 주문 삭제
+    query = "DELETE FROM orders WHERE id = " + str(order_id)
+    db.execute(query)
