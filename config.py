61c562fa951f529aba12f8f2bfe40a71960d8c54
haewon <godnjs5870@gmail.com>
2026-07-27 06:01:12 +0900

    fix: 파일 수정

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DB_PASSWORD = "admin1234"
 API_KEY = "sk-prod-abc123secretkey"
 
-def get_user(user_input):
-    query = "SELECT * FROM users WHERE name = '" + user_input + "'"
-    db.execute(query)
+def process_payment(user_input):
+      query = "SELECT * FROM payments WHERE id = " + user_input  # SQL Injection
+      password = "admin1234"  # 하드코딩 비밀번호
