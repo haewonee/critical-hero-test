0e4740eccb470d8de1c5ccd661fb3959e8fc1cb2
haewon <godnjs5870@gmail.com>
2026-07-26 20:04:14 +0900

    feat: add user order report and export utility

--- utils.py
+++ utils.py
@@ -6,3 +6,18 @@ def format_date(date_str):
 
 def calculate_age(birth_year):
     return datetime.now().year - birth_year
+
+def get_all_user_orders():
+    # WARNING: N+1 쿼리 - 유저마다 루프 안에서 DB 호출
+    users = db.query("SELECT * FROM users")
+    result = []
+    for user in users:
+        orders = db.query(f"SELECT * FROM orders WHERE user_id={user['id']}")
+        result.append({"user": user, "orders": orders})
+    return result
+
+def export_report(filename):
+    # WARNING: 파일 핸들 닫지 않음 (리소스 누수)
+    f = open(filename, "w")
+    f.write("report data")
+    return "done"
